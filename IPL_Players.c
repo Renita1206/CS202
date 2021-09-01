81255976c615a52a1e960309959d9ca127bd1852
Renita Kurian <bluecoder1206@gmail.com>
2021-09-01 08:34:01 +0530

    .

--- IPL_Players.c
+++ IPL_Players.c
@@ -1,5 +1,5 @@
 // Program to create records for IPL players
-// Stores playername, teamname, no of matches played and runs scored
+// Stores playername, teamname, matches played and runs scored
 //   i)Display details of player who scored max runs
 //  ii)Compute no of matches played by each player
 
